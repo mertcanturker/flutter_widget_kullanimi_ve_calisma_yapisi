67585943329bf8a9f7bcfd4a71267b333b9a5899
Mertcan Türker <93304960+mertcanturker@users.noreply.github.com>
2024-12-10 18:37:17 +0300

    calisma yapisi son

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -11,12 +11,18 @@ class Anasayfa extends StatefulWidget {
 
 class _AnasayfaState extends State<Anasayfa> {
   int sayac = 0;
+  bool kontrol = false;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     print("initState() çalıştı");
+  }
+
+  Future<int> toplama(int sayi1,int sayi2) async{
+    int toplam = sayi1 + sayi2;
+    return toplam;
   }
 
   @override
@@ -41,6 +47,34 @@ class _AnasayfaState extends State<Anasayfa> {
                 print("Anasayfaya dönüldü");
               });
             }, child: const Text("Başla")),
+            Visibility(visible: kontrol,child: const Text("Merhaba")),
+            Text(kontrol ? "Merhaba" : "Güle Güle",style: TextStyle(color: kontrol ? Colors.blue : Colors.red ),),
+            ((){
+              if(kontrol){
+                return const Text("Merhaba", style: TextStyle(color: Colors.blue),);
+              }else{
+                return const Text("Güle Güle", style: TextStyle(color: Colors.red),);
+              }
+            }()),
+            ElevatedButton(onPressed: (){
+              setState(() {kontrol = true;});
+            }, child: const Text("Durum 1 (True)")),
+            ElevatedButton(onPressed: (){
+              setState(() {kontrol = false;});
+            }, child: const Text("Durum 2 (False)")),
+            FutureBuilder<int>(
+                future: toplama(10, 20),
+                builder: (context,snapshot){
+                  if(snapshot.hasError){
+                    return const Text("Hata oluştu");
+                  }
+                  if(snapshot.hasData){
+                    return Text("Sonuç : ${snapshot.data}");
+                  }else{
+                    return const Text("Sonuç yok");
+                  }
+                }
+            ),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:calisma_yapisi/anasayfa.dart';
+import 'package:calisma_yapisi/bottom_nav_kullanimi/bottom_navigation_sayfa.dart';
+import 'package:calisma_yapisi/kullanici_etkilesimi/kullanici_etkilisimi_sayfa.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Anasayfa(),
+      home: const KullaniciEtkilisimiSayfa(),
     );
   }
 }
